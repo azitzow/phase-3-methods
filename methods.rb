74f89140d606a7089bf6f419a4f1fa6113e7d10c
Ana Zitzow <anasempai89@gmail.com>
2022-06-06 08:08:59 -0700

    commit tree 05729720700245f565c51d467773e99d39f13562

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greet_programmer(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet_programmer
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Naureen"
+
+def add (num1, num2)
+  num1 + num2
+end
+
+add(1, 2)
+
+def halve(num)
+  if num.class != Integer
+    return nil
+  end
+
+  num/2
+end
+
+halve(6)
+halve("hello")
